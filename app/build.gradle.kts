dc4106159d4ed4ccbec5ab49359d605e3ec74d7d
ralf <ralfoide@gmail.com>
2024-09-06 18:21:53 -0700

    TCM: extractNativeLibs=true is needed to prevent INSTALL_FAILED_INVALID_APK on API 23+.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
 
     defaultConfig {
         applicationId = "com.alflabs.tcm"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 34
         versionCode = 1
         versionName = "0.0.1"
